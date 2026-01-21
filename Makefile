6579163cea7ca06988fee0ea4945040533147018
Edwin Pers <pers.edwin@gmail.com>
2026-01-21 03:49:26 +0000

    json output

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
 discid: discid.c
-	gcc discid.c -ldiscid -o discid
+	gcc discid.c -ldiscid -ljson-c -o discid
 	strip discid
+
+install: discid
+	/usr/bin/install -m 755 discid /usr/local/bin/discid
 
 clean:
 	rm -f discid
+
+deps-ubuntu:
+	apt-get -yq install libdiscid-dev libdiscid0 libjson-c-dev libjson-c5
--- discid.c
+++ discid.c
@@ -21,7 +21,9 @@
 
 --------------------------------------------------------------------------- */
 #include <stdio.h>
+#include <stdlib.h>
 #include <discid/discid.h>
+#include <json-c/json.h>
 
 #ifndef DISCID_HAVE_SPARSE_READ
 #define discid_read_sparse(disc, dev, i) discid_read(disc, dev)
@@ -31,27 +33,42 @@
  * Usage: discid [<device>]
  */
 int main(int argc, char *argv[]) {
-	DiscId *disc = discid_new();
-	int i;
-	char *device = NULL;
-	/* If we have an argument, use it as the device name */
-	if (argc > 1) {
-		device = argv[1];
-	}
-	/* read the disc in the specified (or default) disc drive */
-	if (discid_read_sparse(disc, device, 0) == 0) {
-		fprintf(stderr, "Error: %s\n", discid_get_error_msg(disc));
-		return 1;
-	}
-	printf("First Track: %d\n", discid_get_first_track_num(disc));
-	printf("Last Track: %d\n", discid_get_last_track_num(disc));
-	printf("Sectors: %d\n", discid_get_sectors(disc));
-	for (i = discid_get_first_track_num(disc); i <= discid_get_last_track_num(disc); i++) {
-		printf("Track %-2d: %8d %8d\n", i, discid_get_track_offset(disc, i), discid_get_track_length(disc, i));
-	}
-	printf("FreeDB ID: %s\n", discid_get_freedb_id(disc));
-	printf("MusicBrainz ID: %s\n", discid_get_id(disc));
-	printf("MusicBrainz URL: %s\n", discid_get_submission_url(disc));
-	discid_free(disc);
-	return 0;
+  DiscId *disc = discid_new();
+  int i;
+  char *device = NULL;
+  json_object *root, *tracks;
+
+  /* If we have an argument, use it as the device name */
+  if (argc > 1) {
+    device = argv[1];
+  }
+  /* read the disc in the specified (or default) disc drive */
+  if (discid_read_sparse(disc, device, 0) == 0) {
+    fprintf(stderr, "Error: %s\n", discid_get_error_msg(disc));
+    return 1;
+  }
+
+  root = json_object_new_object();
+  json_object_object_add(root, "first_track", json_object_new_int(discid_get_first_track_num(disc)));
+  json_object_object_add(root, "last_track", json_object_new_int(discid_get_last_track_num(disc)));
+  json_object_object_add(root, "sectors", json_object_new_int(discid_get_sectors(disc)));
+  json_object_object_add(root, "freedb_id", json_object_new_string(discid_get_freedb_id(disc)));
+  json_object_object_add(root, "musicbrainz_id", json_object_new_string(discid_get_id(disc)));
+  json_object_object_add(root, "musicbrainz_submission_url", json_object_new_string(discid_get_submission_url(disc)));
+
+  tracks = json_object_new_array();
+  for (i = discid_get_first_track_num(disc); i <= discid_get_last_track_num(disc); i++) {
+    json_object *track = json_object_new_object();
+    json_object_object_add(track, "number", json_object_new_int(i));
+    json_object_object_add(track, "offset", json_object_new_int(discid_get_track_offset(disc, i)));
+    json_object_object_add(track, "length", json_object_new_int(discid_get_track_length(disc, i)));
+    json_object_array_add(tracks, track);
+  }
+  json_object_object_add(root, "tracks", tracks);
+
+  printf("%s\n", json_object_to_json_string_ext(root, JSON_C_TO_STRING_PRETTY));
+
+  discid_free(disc);
+  json_object_put(root);
+  return 0;
 }
